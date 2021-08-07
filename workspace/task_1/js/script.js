f312161a19cd8f3cb3ecbc22ab7dbfb023a75e01
Vlad <doc200021@gmail.com>
2021-08-07 20:40:29 +0200

    refactor

--- workspace/task_1/js/script.js
+++ workspace/task_1/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms  = +prompt("Скольkо фильмов ты посмотрел?", "");
 
 const personalMovieDB = {
@@ -8,12 +10,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const a  = prompt("Последний фильм?", ""),
-      b  = prompt("На сколько оцените фильм?", ""),
-      c  = prompt("Последний фильм?", ""),
-      d  = prompt("На сколько оцените фильм?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i=0; i<2; i++) {
+    const a  = prompt("Последний фильм?", "");
+    if(a == "" || a == null || a.length >=50) {
+        i--;
+        continue;
+    }
+
+    const b  = prompt("На сколько оцените фильм?", "");
+    if(b == "" || b == null || b.length >=50) {
+        i--;
+        continue;
+    } 
+    personalMovieDB.movies[a] = b;
+}
+
+let mess;
+
+    if(personalMovieDB.count<10) {
+        mess = alert("Просмотрено мало фильмов");
+    } else if(personalMovieDB.count>=10 && personalMovieDB.count<=30) {
+        mess = alert("Вы класический зритель");
+    } else if(personalMovieDB.count>30) {
+        mess = prompt("Вы киноман!");
+    } else {
+        mess = prompt("ERROR");
+    }
+
+
 
 console.log(personalMovieDB);
